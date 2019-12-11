02226cdd2b64ff22d86acbc13f2f7f6690b097ff
2class-007 <2class-007@2class-007-PC>
2019-12-11 17:05:52 +0900

    dafdsfdsf

--- 짜잔/src/Test/test.java
+++ 짜잔/src/Test/test.java
@@ -3,7 +3,7 @@ package Test;
 public class test {
  public static void main(String[] args) {
 	System.out.println("ddd");
-	System.out.println("무한으로 즐겨요 명륜진사 갈비");
+	System.out.println("무즐명갈");
 	System.out.println("ddd");
 	System.out.println("ddd");
 	System.out.println("ddd");
@@ -16,12 +16,7 @@ public class test {
 	System.out.println("ddd");
 	System.out.println("ddd");
 	
-	System.out.println("ddd");
-	System.out.println("무한으로 즐겨요 명륜진사 갈비");
-	System.out.println("ddd");
-	System.out.println("ddd");
-	System.out.println("ddd");
-	System.out.println("ddd");
+	
 	
 }
 }
