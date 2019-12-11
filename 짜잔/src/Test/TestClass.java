cab65b46c429644f273aedff093aa0eeb25c7ede
2class-006 <2class-006@2class-006-PC>
2019-12-11 16:43:31 +0900

    as

--- 짜잔/src/Test/TestClass.java
+++ 짜잔/src/Test/TestClass.java
@@ -5,6 +5,9 @@ public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("띠로리");
 		System.out.println("포로리");
+		System.out.println("띠로리");
+		System.out.println("포로리");System.out.println("띠로리");
+		System.out.println("포로리");
 
 	}
 
