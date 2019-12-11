3830e09a577e7a4d00376be7afc47f0f8011de1e
2class-006 <2class-006@2class-006-PC>
2019-12-11 17:13:24 +0900

    # WARNING: head commit changed in the meantime
    1234

--- 짜잔/src/Test/test.java
+++ 짜잔/src/Test/test.java
@@ -8,7 +8,14 @@ public class test {
 	System.out.println("ddd");
 	System.out.println("ddd");
 	System.out.println("ddd");
-	
+	System.out.println("무한으로 즐겨요 명륜진사 갈비");
+	System.out.println("무한으로 즐겨요 명륜진사 갈비");
+	System.out.println("무한으로 즐겨요 명륜진사 갈비");
+	System.out.println("무한으로 즐겨요 명륜진사 갈비");
+	System.out.println("무한으로 즐겨요 명륜진사 갈비");
+	System.out.println("무한으로 즐겨요 명륜진사 갈비");
+	System.out.println("무한으로 즐겨요 명륜진사 갈비");
+	System.out.println("무한으로 즐겨요 명륜진사 갈비");
 	System.out.println("ddd");
 	System.out.println("무한으로 즐겨요 명륜진사 갈비");
 	System.out.println("ddd");
