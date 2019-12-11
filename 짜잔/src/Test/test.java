770c084d7ad29def517bb01587d311587c159056
SK O <mnbvcxzjjkk312@hotmail.com>
2019-12-11 16:47:35 +0900

    aa

--- 짜잔/src/Test/test.java
+++ 짜잔/src/Test/test.java
@@ -4,5 +4,10 @@ public class test {
  public static void main(String[] args) {
 	System.out.println("ddd");
 	System.out.println("무한으로 즐겨요 명륜진사 갈비");
+	System.out.println("ddd");
+	System.out.println("ddd");
+	System.out.println("ddd");
+	System.out.println("ddd");
+	
 }
 }
