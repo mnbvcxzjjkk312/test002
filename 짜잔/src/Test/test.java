80e68f66027b508d882d27645b89f94516d79f4b
2class-015 <2class-015@2class-015-PC>
2019-12-11 17:17:38 +0900

    펭하

--- 짜잔/src/Test/test.java
+++ 짜잔/src/Test/test.java
@@ -3,20 +3,7 @@ package Test;
 public class test {
  public static void main(String[] args) {
 	System.out.println("ddd");
-	System.out.println("¹«Áñ¸í°¥");
-	System.out.println("ddd");
-	System.out.println("ddd");
-	System.out.println("ddd");
-	System.out.println("ddd");
-	
-	System.out.println("ddd");
-	System.out.println("¹«ÇÑÀ¸·Î Áñ°Ü¿ä ¸í·ûÁø»ç °¥ºñ");
-	System.out.println("ddd");
-	System.out.println("ddd");
-	System.out.println("ddd");
-	System.out.println("ddd");
-	
-	
+	System.out.println("ÆëÇÏ~");
 	
 }
 }
